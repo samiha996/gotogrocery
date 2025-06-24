658424f2d3cbeba8e05ea353194015508232bd3c
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-06-24 10:16:14 +0300

    Update sale_order_line.py

--- product_package_price/models/sale_order_line.py
+++ product_package_price/models/sale_order_line.py
@@ -1,11 +1,36 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     new_qty = fields.Float(string="New Qty")
+    package_price = fields.Float(string="Package Price")
+    _package_price_initialized = fields.Boolean(default=False)
+
+    @api.onchange('product_id')
+    def _onchange_product_id_set_first_packaging(self):
+        for line in self:
+            if line.product_id and not line.product_packaging_id and line.product_id.packaging_ids:
+                line.product_packaging_id = line.product_id.packaging_ids[0]
+
+    @api.onchange('product_packaging_id')
+    def _onchange_product_packaging_id(self):
+        for line in self:
+            if line.product_packaging_id:
+                if not line._package_price_initialized:
+                    line.package_price = line.product_packaging_id.package_price or 0.0
+                    line._package_price_initialized = True
+            line._update_qty_and_prices()
+
+    @api.onchange('product_packaging_qty')
+    def _onchange_packaging_qty(self):
+        for line in self:
+            line._update_qty_and_prices()
+
+    @api.onchange('package_price')
+    def _onchange_package_price(self):
+        for line in self:
+            line._update_qty_and_prices()
 
     @api.onchange('new_qty')
     def _onchange_new_qty(self):
@@ -13,20 +38,12 @@ class SaleOrderLine(models.Model):
             if line.new_qty:
                 line.product_uom_qty += line.new_qty
 
-
-    @api.depends('product_id', 'product_uom', 'product_uom_qty', 'product_packaging_id',)
-    def _compute_price_unit(self):
-        super()._compute_price_unit()
+    def _update_qty_and_prices(self):
         for line in self:
-            if line.product_packaging_id and line.product_packaging_id.unit_price:
-                line.price_unit = line.product_packaging_id.unit_price
-            else:
-                line.price_unit = line.price_unit
+            pieces_per_box = line.product_packaging_id.qty or 1.0
+            num_boxes = line.product_packaging_qty or 0.0
+            total_pieces = pieces_per_box * num_boxes
 
-    
-    @api.onchange('product_packaging_id')
-    def _onchange_product_packaging_id(self):
-        if self.product_packaging_id and self.product_packaging_id.qty:
-            self.product_uom_qty = self.product_packaging_id.qty
-        else:
-            self.product_uom_qty = self.product_uom_qty
+           
+            line.product_uom_qty = total_pieces
+            line.price_unit = line.package_price / pieces_per_box if pieces_per_box else 0.0
