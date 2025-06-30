7f62c637798b2b644c37a9685c0afa248d3f3260
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-06-30 16:27:56 +0300

    Add files via upload

---
+++ product_package_price/models/stock_move.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    package_qty = fields.Float(string="Package Quantity")
+    pieces_qty = fields.Float(string="Pieces Quantity")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'package_qty' not in vals and vals.get('sale_line_id'):
+                sale_line = self.env['sale.order.line'].browse(vals['sale_line_id'])
+                vals['package_qty'] = sale_line.product_packaging_qty
+                vals['pieces_qty'] = sale_line.new_qty or 0.0
+        return super().create(vals_list)
