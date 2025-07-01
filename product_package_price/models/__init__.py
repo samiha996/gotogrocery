c261fe89cf40b0f31de9dc23b6c2e16a90d2d29a
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-07-01 23:39:38 +0300

    Update __init__.py

--- product_package_price/models/__init__.py
+++ product_package_price/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import res_company, stock_move, account_move_line
+from . import res_partner,res_company, stock_move, account_move_line
 from . import purchase_order,product_package, purchase_order_line, sale_order_line
