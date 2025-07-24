65079e5692df7ac370eeb3b40af1372d8a987d54
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-07-24 13:28:14 +0300

    Update __init__.py

--- product_package_price/models/__init__.py
+++ product_package_price/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import res_partner,res_company, stock_move, account_move_line
 from . import purchase_order,product_package, purchase_order_line, sale_order_line
+from . import product_template
